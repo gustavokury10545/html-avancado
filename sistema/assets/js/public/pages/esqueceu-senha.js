6423f11b759841e478d76a2df85cb42832f1828a
gustavokury10545 <gustavokuri51@gmail.com>
2025-08-16 18:13:51 -0300

    home de apresentação do sistema

---
+++ sistema/assets/js/public/pages/esqueceu-senha.js
@@ -0,0 +1,12 @@
+document.getElementById("forgotForm").addEventListener("submit", function (e){
+    e.preventDefault();
+
+    const email = document.getElementById("email").value.trim();
+    if(email === "aluno@hotmail.com"){
+        window.location.href = "trocar-senha.html";
+    } else if(email === ""){
+        alert("E-mail deve ser preenchido");
+    } else {
+        alert("E-mail inválido");
+    }
+});
